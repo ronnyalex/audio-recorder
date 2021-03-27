464eab8dc3cd2f99f84a37862988ff693f8c7162
ronnyalex <ronnyalex@outlook.com>
2021-03-27 10:04:41 +0100

    Settings made

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <AudioRecorder />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue'
+import AudioRecorder from './components/AudioRecorder.vue'
 
 export default Vue.extend({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+    AudioRecorder,
   },
-});
+})
 </script>
 
 <style lang="less">
---
+++ src/components/AudioRecorder.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>hej</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  // components: {
+  //   New,
+  // },
+  props: {
+    obj: { type: Object, default: () => ({}) },
+    arr: { type: Array, default: () => [] },
+    num: { type: Number, default: null },
+    str: { type: String, default: '' },
+  },
+  data() {
+    return {
+      key: '',
+    }
+  },
+  created() {
+    console.log('hej')
+  },
+  methods: {},
+})
+</script>
+<style lang="less" scoped></style>
